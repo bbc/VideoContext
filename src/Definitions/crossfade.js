9bd30cf7686196f70095372ef9f0e86cbee02229
Matthew Shotton <matthew.shotton@bbc.co.uk>
2017-03-13 15:57:24 +0000

    fixed the direction which transition nodes transition from/to

--- src/Definitions/crossfade.js
+++ src/Definitions/crossfade.js
@@ -19,14 +19,14 @@ let crossfade = {
     void main(){\
         vec4 color_a = texture2D(u_image_a, v_texCoord);\
         vec4 color_b = texture2D(u_image_b, v_texCoord);\
-        color_a[0] *= mix;\
-        color_a[1] *= mix;\
-        color_a[2] *= mix;\
-        color_a[3] *= mix;\
-        color_b[0] *= (1.0 - mix);\
-        color_b[1] *= (1.0 - mix);\
-        color_b[2] *= (1.0 - mix);\
-        color_b[3] *= (1.0 - mix);\
+        color_a[0] *= (1.0 - mix);\
+        color_a[1] *= (1.0 - mix);\
+        color_a[2] *= (1.0 - mix);\
+        color_a[3] *= (1.0 - mix);\
+        color_b[0] *= mix;\
+        color_b[1] *= mix;\
+        color_b[2] *= mix;\
+        color_b[3] *= mix;\
         gl_FragColor = color_a + color_b;\
     }",
     "properties":{
