63302234d174f681d6dec75e622f5add320ea063
Matthew Shotton <matthew.shotton@bbc.co.uk>
2017-06-19 14:00:52 +0100

    changed build process to output a single UMD module

--- docs/dist/videocontext.js
+++ docs/dist/videocontext.js
@@ -1,5 +1,14 @@
-var VideoContext =
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["VideoContext"] = factory();
+	else
+		root["VideoContext"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -5980,5 +5989,7 @@ var VideoContext =
 	module.exports = exports["default"];
 
 /***/ })
-/******/ ]);
+/******/ ])
+});
+;
 //# sourceMappingURL=videocontext.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     output: {
         path: __dirname+'/dist',
         filename: "videocontext.js", 
-        libraryTarget: "var",
+        libraryTarget: "umd",
         library: "VideoContext"
     },
     module: {
