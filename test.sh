9156e1b17167ebe44b45167ae51eb7b8f0f36a71
Peter Taylour <peter.taylour@bbc.co.uk>
2018-07-06 18:14:59 +0100

    update test scripts for ported integration tests

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
-#Run unit tests
-
 set -o monitor
+
 echo "###############################"
 echo "# Runing Available Unit Tests #"
 echo "###############################"
@@ -17,36 +16,4 @@ echo "######################################"
 echo "# Runing Available Integration Tests #"
 echo "######################################"
 
-echo "Starting webserver..."
-
-# Start a webserver to view the integration tests
-node ./node_modules/http-server/bin/http-server &
-
-echo "Detecting OS..."
-
-# Detect the OS
-# Credit for this - http://stackoverflow.com/questions/394230/detect-the-os-from-a-bash-script
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-   platform='linux'
-fi
-
-# Arbitrary time for http-server to start - FixMe
-sleep 1
-
-echo "Opening browser to run integration tests..."
-
-# Run integration tests
-if [[ $platform == 'linux' ]]; then
-    xdg-open http://localhost:8080/test/integration/
-elif [[ $platform == 'unknown' ]]; then
-    open http://localhost:8080/test/integration/
-fi
-
-echo
-echo "Bring webserver process to foreground...(CTRL+C to kill)"
-echo
-
-# Bring the server process to the foreground so it can be killed
-fg %1
+npm run test-integration
