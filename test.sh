1ef8baff0d9b91a22348d060368fdc82c7ff7651
Peter Taylour <peter.taylour@bbc.co.uk>
2018-07-09 10:53:31 +0100

    setup parrallel test suites for travis
    https://docs.travis-ci.com/user/speeding-up-the-build/

--- test.sh
+++ test.sh
@@ -1,19 +1,30 @@
 set -o monitor
 
-echo "###############################"
-echo "# Runing Available Unit Tests #"
-echo "###############################"
+if [ "$TEST_SUITE" == "unit" ]
+  then
+      echo "###############################"
+      echo "# Runing Available Unit Tests #"
+      echo "###############################"
 
-npm run test-coverage
+      npm run test-coverage
 
-echo "#####################################"
-echo "# Runing Available Regression Tests #"
-echo "#####################################"
+elif [ "$TEST_SUITE" == "integration" ]
+  then
 
-npm run test-regression
+      echo "######################################"
+      echo "# Runing Available Integration Tests #"
+      echo "######################################"
 
-echo "######################################"
-echo "# Runing Available Integration Tests #"
-echo "######################################"
+      npm run test-integration
 
-npm run test-integration
+elif [ "$TEST_SUITE" == "regression" ]
+  then
+      echo "#####################################"
+      echo "# Runing Available Regression Tests #"
+      echo "#####################################"
+
+      npm run test-regression
+
+else
+    echo "TEST_SUITE env should be set to unit, integration or regression"
+fi
