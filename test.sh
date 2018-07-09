ed65954c99e43e7b06c10b59e0c03f2295839b8c
Peter Taylour <peter.taylour@bbc.co.uk>
2018-07-09 12:56:15 +0100

    add a travis test that lints and builds package

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -o monitor
 if [ "$TEST_SUITE" == "unit" ]
   then
       echo "###############################"
-      echo "# Runing Available Unit Tests #"
+      echo "# Running Available Unit Tests #"
       echo "###############################"
 
       npm run test-coverage
@@ -12,7 +12,7 @@ elif [ "$TEST_SUITE" == "integration" ]
   then
 
       echo "######################################"
-      echo "# Runing Available Integration Tests #"
+      echo "# Running Available Integration Tests #"
       echo "######################################"
 
       npm run test-integration
@@ -20,10 +20,19 @@ elif [ "$TEST_SUITE" == "integration" ]
 elif [ "$TEST_SUITE" == "regression" ]
   then
       echo "#####################################"
-      echo "# Runing Available Regression Tests #"
+      echo "# Running Available Regression Tests #"
       echo "#####################################"
 
       npm run test-regression
+
+elif [ "$TEST_SUITE" == "build" ]
+  then
+    echo "#####################################"
+    echo "# Linting and Building package #"
+    echo "#####################################"
+
+    npm run lint
+    npm run build
 
 else
     echo "exit 1. TEST_SUITE env should be set to unit, integration or regression"
