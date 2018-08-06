df45e4e3e8822f232fe58ef1ffe66e811f1f5b2c
Peter Taylour <peter.taylour@bbc.co.uk>
2018-08-06 17:21:49 +0100

    add integration tests for medianode volume attribute

---
+++ test/integration/medianode.test.js
@@ -0,0 +1,61 @@
+import VideoContext from "../../src/videocontext";
+
+let ctx;
+require("webgl-mock");
+
+const nodeFactory = (vidCtx, attr = {}) => {
+    const element = {
+        play: jest.fn(),
+        pause: jest.fn()
+    };
+    const node = vidCtx.video(element, undefined, undefined, attr);
+    // some sane defaults
+    // node should play from the start
+    node.start(0);
+    node.stop(10);
+    return { node, element };
+};
+
+beforeEach(() => {
+    const canvas = new HTMLCanvasElement(500, 500);
+    ctx = new VideoContext(canvas, undefined, { useVideoElementCache: false });
+});
+
+describe("medianode", () => {
+    describe("attributes", () => {
+        it("volume setter sets volume on element", () => {
+            const { element, node } = nodeFactory(ctx);
+            node.volume = 0.5;
+            expect(element.volume).toBe(0.5);
+            node.volume = 1.5;
+            expect(element.volume).toBe(1.5);
+        });
+
+        it("should play with a default volume if volume attribute is not supplied", () => {
+            expect.assertions(3);
+
+            const { element } = nodeFactory(ctx);
+            expect(element.volume).toBe(undefined /* because our mock has no volume attr */);
+
+            // We want to trigger a load so that the node attributes will be applied to
+            // the video element.
+            // advance timeline 1s to do this
+            ctx.update(1);
+
+            expect(element.volume).not.toBe(undefined /* it will be a number now */);
+            expect(element.volume).toBeGreaterThan(-Infinity);
+        });
+
+        it("should set provided volume on update if volume attribute is supplied", () => {
+            const { element } = nodeFactory(ctx, { volume: 0.2 });
+            expect(element.volume).toBe(undefined /* because our mock has no volume attr */);
+
+            // We want to trigger a load so that the node attributes will be applied to
+            // the video element.
+            // advance timeline 1s to do this
+            ctx.update(1);
+
+            expect(element.volume).toBe(0.2);
+        });
+    });
+});
