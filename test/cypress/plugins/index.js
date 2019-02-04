8b57add4eb676ef2c1d05e5d5c8d3cf48293b93c
Peter Taylour <peter.taylour@bbc.co.uk>
2019-02-04 16:42:03 +0000

    add eslint plugin for cypress

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,5 +1,6 @@
 const { addMatchImageSnapshotPlugin } = require("cypress-image-snapshot/plugin");
 
+// eslint-disable-next-line
 module.exports = (on, config) => {
     addMatchImageSnapshotPlugin(on, config);
 };
