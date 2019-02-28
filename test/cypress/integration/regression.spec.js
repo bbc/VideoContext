88c6d6cf15d5de8566cea6d4f4e129c22d0281b7
Peter Taylour <peter.taylour@bbc.co.uk>
2019-02-28 18:24:41 +0000

    big improvement! frame accurate!

--- test/cypress/integration/regression.spec.js
+++ test/cypress/integration/regression.spec.js
@@ -2,61 +2,46 @@
  * to update snapshots run: `yarn run cypress:update`
  */
 
-context("playback tests", () => {
-    /**
-     * @param {String} configuration.file - the .html file to be tested.
-     * @param {String} configuration.waitTime - time in ms to wait between action and screenshot
-     */
-    const playbackTest = ({ file, waitTime }) => {
-        cy.visit(file);
-
-        console.log("todo, remove waitTime", waitTime);
-
-        // TODO must pull out in a way that ensure the event listeners are added first
-        const resolveAfterFrames = num =>
-            new Promise(resolve => {
-                let updateCount = 0;
-                cy.window().then(win => {
-                    win.addEventListener("cy:update", () => {
-                        updateCount++;
-                        if (updateCount === num) {
-                            resolve();
-                        }
-                    });
-                });
-            });
-
-        const afterFirstFrame = resolveAfterFrames(1);
-        const afterAFewSeconds = resolveAfterFrames(25 * 3);
-
-        cy.get("#play").click();
-
-        cy.window()
-            .then(() => console.log("play"))
-            .then(() => afterFirstFrame);
-
-        cy.window().then(() => console.log("waited for play"));
-
-        cy.get("#canvas").matchImageSnapshot("after play");
-
-        // pause after a wait
-        // should count ticks here instead
-        // cy.wait(waitTime);
-
-        cy.window()
-            .then(() => console.log("waited"))
-            .then(() => afterAFewSeconds);
-
-        cy.get("#pause").click();
-
-        // check snapshot after pause
-        return cy.get("#canvas").matchImageSnapshot("after pause");
-    };
-
-    it("playback.html", () => {
-        playbackTest({
-            file: "playback.html",
-            waitTime: 1000
+const takeScreenShotAtTime = (time, { ctx }) =>
+    new Promise(resolve => {
+        ctx.registerTimelineCallback(time, () => {
+            ctx.pause();
+            resolve(
+                // we must return a cypress chain
+                cy
+                    .get("#canvas")
+                    .matchImageSnapshot(`at time ${time}`)
+                    .then(() => {
+                        ctx.play();
+                    })
+            );
         });
+    });
+
+// TODO we should also define the pipeline like this
+// only dep in html should be the canvas and window.VideoContext
+const takeScreenShotAtTimes = (times = [1, 25, 50]) => {
+    let window;
+    let cyPromise = cy.window().then(win => {
+        win.ctx.play();
+        window = win;
+    });
+
+    times.forEach(time => {
+        cyPromise = cyPromise.then(() => takeScreenShotAtTime(time, { ctx: window.ctx }));
+    });
+
+    return cyPromise.then(() => {
+        window.ctx.reset();
+    });
+};
+
+context("playback tests", () => {
+    it("playback.html", () => {
+        cy.visit("playback.html");
+
+        // use cy.window to put tests here
+
+        takeScreenShotAtTimes([0.1, 1, 2]);
     });
 });
