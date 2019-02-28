30eec89eb0ebd2fa72defc349aec0eee7b1b439c
Peter Taylour <peter.taylour@bbc.co.uk>
2019-02-28 12:44:32 +0000

    make progress towards frame accurate snapshots

--- test/cypress/integration/regression.spec.js
+++ test/cypress/integration/regression.spec.js
@@ -10,16 +10,43 @@ context("playback tests", () => {
     const playbackTest = ({ file, waitTime }) => {
         cy.visit(file);
 
+        console.log("todo, remove waitTime", waitTime);
+
+        // TODO must pull out in a way that ensure the event listeners are added first
+        const resolveAfterFrames = num =>
+            new Promise(resolve => {
+                let updateCount = 0;
+                cy.window().then(win => {
+                    win.addEventListener("cy:update", () => {
+                        updateCount++;
+                        if (updateCount === num) {
+                            resolve();
+                        }
+                    });
+                });
+            });
+
+        const afterFirstFrame = resolveAfterFrames(1);
+        const afterAFewSeconds = resolveAfterFrames(25 * 3);
+
         cy.get("#play").click();
 
-        // give videocontext a chance to render first frame
-        // should try and use the vidctx events here
-        cy.wait(100);
+        cy.window()
+            .then(() => console.log("play"))
+            .then(() => afterFirstFrame);
+
+        cy.window().then(() => console.log("waited for play"));
 
         cy.get("#canvas").matchImageSnapshot("after play");
 
         // pause after a wait
-        cy.wait(waitTime);
+        // should count ticks here instead
+        // cy.wait(waitTime);
+
+        cy.window()
+            .then(() => console.log("waited"))
+            .then(() => afterAFewSeconds);
+
         cy.get("#pause").click();
 
         // check snapshot after pause
