4264bc1178e85c1145f527f63e8023b9d8d9ba77
Peter Taylour <peter.taylour@bbc.co.uk>
2019-03-13 11:33:47 +0000

    port all playback tests

--- test/cypress/integration/regression.spec.js
+++ test/cypress/integration/regression.spec.js
@@ -19,8 +19,6 @@ const takeScreenShotAtTime = (time, { ctx, id }) =>
         });
     });
 
-// TODO we should also define the pipeline like this
-// only dep in html should be the canvas and window.VideoContext
 const takeScreenShotAtTimes = (times = [1, 25, 50], { id }) => {
     // use a closure to access window
     let window;
@@ -45,11 +43,13 @@ const takeScreenShotAtTimes = (times = [1, 25, 50], { id }) => {
     });
 };
 
+beforeEach(() => {
+    cy.visit("index.html");
+    cy.viewport("macbook-11");
+});
+
 context("playback tests", () => {
     it("plays back video", () => {
-        cy.visit("index.html");
-        cy.viewport("macbook-11");
-
         // set up VideoContext
         cy.window().then(({ ctx }) => {
             const videoNode = ctx.video("../assets/video1.webm"); // path relative to index.html
@@ -58,6 +58,55 @@ context("playback tests", () => {
         });
 
         // check output  match the snapshots at times on the timeline
-        takeScreenShotAtTimes([0.5, 1, 1.5], { id: "playback" });
+        takeScreenShotAtTimes([0.5, 1, 1.5], { id: "playback-video" });
+    });
+
+    it("plays back image", () => {
+        // set up VideoContext
+        cy.window().then(({ ctx }) => {
+            const imageNode = ctx.image("../assets/test-image.png");
+            imageNode.startAt(0);
+            imageNode.connect(ctx.destination);
+        });
+
+        // check output  match the snapshots at times on the timeline
+        takeScreenShotAtTimes([0.5, 1, 1.5], { id: "playback-image" });
+    });
+
+    it("plays back image with no creatImageBitmap", () => {
+        // set up VideoContext
+        cy.window().then(win => {
+            const ctx = win.ctx;
+            // remove createImageBitmap to simulate being in a browser with no support
+            win.createImageBitmap = undefined;
+
+            const imageNode = ctx.image("../assets/test-image.png");
+            imageNode.startAt(0);
+            imageNode.connect(ctx.destination);
+        });
+
+        // check output  match the snapshots at times on the timeline
+        takeScreenShotAtTimes([0.5, 1, 1.5], {
+            id: "playback-image-with-no-createImageBitmap"
+        });
+    });
+
+    it("plays back  with user supplied element and start offset", () => {
+        // set up VideoContext
+        cy.window().then(win => {
+            const ctx = win.ctx;
+            const video = win.document.createElement("video");
+            video.src = "../assets/video1.webm";
+            video.crossOrigin = "anonymous";
+
+            const sourceOffset = 10;
+            const videoNode = ctx.video(video, sourceOffset);
+
+            videoNode.startAt(0);
+            videoNode.connect(ctx.destination);
+        });
+
+        // check output  match the snapshots at times on the timeline
+        takeScreenShotAtTimes([0.5, 1, 1.5], { id: "playback-user-supplied-element" });
     });
 });
