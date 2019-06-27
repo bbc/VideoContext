ebfff85733f6d69546d721ffdb07a674848e67ac
Richard Najdowski <richard.najdowski@bbc.co.uk>
2019-06-27 13:59:48 +0100

    Improve the `takeScreenShotAtTime` command precision. Allow snapshot option overrides.

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -4,7 +4,7 @@ import videoContextScreenShotsAtTimesCommand from "./videoContextScreenShotsAtTi
 addMatchImageSnapshotCommand({
     failureThreshold: 0.06, // threshold for entire image
     failureThresholdType: "percent", // percent of image or number of pixels
-    customDiffConfig: { threshold: 0.2 }, // threshold for each pixel
+    customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
     capture: "viewport", // capture viewport in screenshot
     disableTimersAndAnimations: false // we always pause before taking screenshots
 });
