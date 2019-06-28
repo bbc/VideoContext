706d0da39201b03038d4d250c57727d1be944634
Peter Taylour <peter.taylour@bbc.co.uk>
2019-06-28 14:42:10 +0100

    set up cypress dashboard

---
+++ cypress-run.sh
@@ -0,0 +1,11 @@
+# Perform the command `cypress run`
+# record the run if `CYPRESS_RECORD_KEY` is set
+# this way we'll only CI will record runs
+# and no non-robots require the record key.
+
+if [ -z "$CYPRESS_RECORD_KEY" ]
+then
+    ./node_modules/.bin/cypress run --record
+else
+    ./node_modules/.bin/cypress run
+fi
