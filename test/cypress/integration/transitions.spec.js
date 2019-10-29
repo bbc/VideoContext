5c20ce45a59535fb6aceb867dda998e48cd58330
Richard Najdowski <richard.najdowski@bbc.co.uk>
2019-10-29 17:00:22 +0000

    Revert star wipe failure threshold - travis error was a timeout, rather than non-match

--- test/cypress/integration/transitions.spec.js
+++ test/cypress/integration/transitions.spec.js
@@ -45,13 +45,8 @@ const setupTransitionPipelineForDefinition = definitionName => ({ ctx, VideoCont
     { definitionName: "VERTICAL_WIPE" },
     { definitionName: "RANDOM_DISSOLVE" },
     { definitionName: "TO_COLOR_AND_BACK" },
-    // These are very frame dependent so we allow for inconsistent runs
-    {
-        definitionName: "STAR_WIPE",
-        options: {
-            failureThreshold: 0.3 // threshold for entire image
-        }
-    },
+    { definitionName: "STAR_WIPE" },
+    // This is very frame dependent so we allow for inconsistent runs
     {
         definitionName: "DREAMFADE",
         options: {
